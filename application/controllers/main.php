b94d755490968453ded255de8bcc6f6b9df193c9
harry <twharryhung@gmail.com>
2015-04-16 17:55:55 +0800

    資料庫表建立完成

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -14,4 +14,32 @@ class Main extends Site_controller {
   public function index () {
     $this->load_view (null);
   }
+
+  public function add () {
+    $this->load_view ();
+  }
+
+  public function create () {
+    $account = $this->input_post ('account');
+    $password = $this->input_post ('password');
+    $name = $this->input_post ('name');
+
+    // 過濾
+    $account = filter_var($account, FILTER_SANITIZE_STRING);
+    $password = filter_var($password, FILTER_SANITIZE_STRING);
+    $name = filter_var($name, FILTER_SANITIZE_STRING);
+
+    if (!$account || !$password || !$name) {
+      // 檢查是否有錯誤
+
+      redirect (array ('main', 'index'));
+    }
+
+    if (verifyCreateOrm (User::create (array('account' => $account, 'password' => $password, 'name' => $name)))) {
+      redirect (array('main', 'index', '註冊成功'));
+    } else {
+      redirect (array('main', 'index', '註冊失敗'));
+
+    }
+  }
 }
--- application/views/content/site/main/index/content.php
+++ application/views/content/site/main/index/content.php
@@ -1,1 +1,20 @@
-Hello World!
+<h1>登入</h1>
+
+<form action="<?php echo base_url ('main', 'info'); ?>">
+  <div>
+    <div>
+      <span>帳號：</span>
+      <input type='text' name='account'>
+    </div>
+    <div>
+      <span>密碼：</span>
+      <input type='password' name='password'>
+    </div>
+  </div>
+  <div>
+    <button type='submit'>送出</button>
+  </div>
+</form>
+<div>
+  <a href="<?php echo base_url ('main', 'addUser'); ?>"></a>
+</div>
